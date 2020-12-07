dceafeaac8b8b2550565000e34410ae0b84cae12
Armchair Traveller <sentient.sober.armchair@gmail.com>
2020-12-06 23:30:56 -0800

    version bump

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,13 @@
 // Consult https://www.snowpack.dev to learn about these options
 module.exports = {
-  extends: "@sveltejs/snowpack-config",
   buildOptions: {
     souceMaps: true,
+  },
+  extends: "@sveltejs/snowpack-config",
+  mount: {
+    "src/components": "/_components",
+  },
+  alias: {
+    $components: "./src/components",
   },
 };
